c37cd76b147f0563c2c712a497925af1992ee192
stormstina <stina.nerin@gmail.com>
2023-05-13 10:19:53 +0200

    Add render profile function

---
+++ public/js/profilePage.js
@@ -0,0 +1,17 @@
+const renderProfile = async(id) => {
+    const profile = await fetchData(`/api/person/${id}`)
+    console.log(profile);
+
+    if (profile) {
+        const div = createElement("div")
+        const name = createElement("h1", "", profile.name)
+        const id = createElement("p", "", `Account #${id}`)
+        const amount = createElement("p", "", `Amount $${profile.amount}`)
+        div.append(name, id, amount)
+        document.body.append(div)
+
+    } else {
+        // todo! Person not found
+    }
+}
+

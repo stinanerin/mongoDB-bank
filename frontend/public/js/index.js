648ebb1322ce2f734b4631ce0860797a62392d08
stormstina <stina.nerin@gmail.com>
2023-05-13 15:52:33 +0200

    Add redirect of unspecified route to home page

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,15 +1,16 @@
 //! new
 
-
-
 // asynkron laddar innehåller för varje view/route/path
 const router = async () => {
     const routes = [
         // root path - view:
 
         { path: "/", view: () => console.log("viewing home") },
-        { path: "/accounts", view: () => console.log("viewing home") },
-        { path: "/accounts/:id", view: () => console.log("viewing home") },
+        { path: "/accounts", view: () => console.log("viewing accounts") },
+        {
+            path: "/accounts/:id",
+            view: () => console.log("viewing accounts/:id"),
+        },
     ];
 
     // Test each route for potential match
@@ -18,19 +19,26 @@ const router = async () => {
         return {
             route: route,
             // Does the current url location match a route
-            isMatch: location.pathname === route.path
+            isMatch: location.pathname === route.path,
         };
     });
 
     console.log("potentialMatches", potentialMatches);
 
     // Finds the route with the isMatch: true
-    const match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+    let match = potentialMatches.find(
+        (potentialMatch) => potentialMatch.isMatch
+    );
 
-    console.log("match", match);
-
+    // If match is undefined - navigate to home page
+    if (!match) {
+        match = { 
+            route: routes[0], 
+            isMatch: true
+        };
+    }
+    console.log("match", match.route.view());
 };
-
 
 // Listens to DOM loads
 document.addEventListener("DOMContentLoaded", () => {

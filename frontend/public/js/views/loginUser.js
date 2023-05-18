b403ee8a18dedaaf339718bce132dfeb35a5a747
stormstina <stina.nerin@gmail.com>
2023-05-18 15:28:38 +0200

    Add centering of forms

--- frontend/public/js/views/loginUser.js
+++ frontend/public/js/views/loginUser.js
@@ -7,34 +7,36 @@ export default class extends AbstractView {
     }
     async getHtml() {
         return `
-            <form class="form" id="loginForm">
-                <h2>Log in</h2>
-                <div class="form-group">
-                    <label for="loginName" class="form-label">
-                        Username<sup>*</sup>
-                    </label>
-                    <input
-                        class="form-control"
-                        type="text"
-                        id="loginName"
-                        placeholder="Enter username"
-                        required
-                    />
-                </div>
-                <div class="form-group">
-                    <label for="loginPwd" class="form-label">
-                        Password<sup>*</sup>
-                    </label>
-                    <input
-                        class="form-control"
-                        type="password"
-                        id="loginPwd"
-                        placeholder="Enter password"
-                        required
-                    />
-                </div>
-                <button class="btn">Log in</button>
-            </form>`;
+            <div class="form-wrapper">
+                <form class="form" id="loginForm">
+                    <h2>Log in</h2>
+                    <div class="form-group">
+                        <label for="loginName" class="form-label">
+                            Username<sup>*</sup>
+                        </label>
+                        <input
+                            class="form-control"
+                            type="text"
+                            id="loginName"
+                            placeholder="Enter username"
+                            required
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="loginPwd" class="form-label">
+                            Password<sup>*</sup>
+                        </label>
+                        <input
+                            class="form-control"
+                            type="password"
+                            id="loginPwd"
+                            placeholder="Enter password"
+                            required
+                        />
+                    </div>
+                    <button class="btn">Log in</button>
+                </form>
+            </div>`;
     }
     addEventListeners() {
         document
--- frontend/public/js/views/registerUser.js
+++ frontend/public/js/views/registerUser.js
@@ -7,34 +7,36 @@ export default class extends AbstractView {
     }
     async getHtml() {
         return `
-            <form class="form" id="registerForm">
-                <h2>Register</h2>
-                <div class="form-group">
-                    <label for="registerName" class="form-label">
-                        Username<sup>*</sup>
-                    </label>
-                    <input
-                        class="form-control"
-                        type="text"
-                        id="registerName"
-                        placeholder="Enter username"
-                        required
-                    />
-                </div>
-                <div class="form-group">
-                    <label for="registerPwd" class="form-label">
-                        Password<sup>*</sup>
-                    </label>
-                    <input
-                        class="form-control"
-                        type="password"
-                        id="registerPwd"
-                        placeholder="Enter password"
-                        required
-                    />
-                </div>
-                <button class="btn">Register</button>
-            </form>`;
+            <div class="form-wrapper">
+                <form class="form" id="registerForm">
+                    <h2>Register</h2>
+                    <div class="form-group">
+                        <label for="registerName" class="form-label">
+                            Username<sup>*</sup>
+                        </label>
+                        <input
+                            class="form-control"
+                            type="text"
+                            id="registerName"
+                            placeholder="Enter username"
+                            required
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="registerPwd" class="form-label">
+                            Password<sup>*</sup>
+                        </label>
+                        <input
+                            class="form-control"
+                            type="password"
+                            id="registerPwd"
+                            placeholder="Enter password"
+                            required
+                        />
+                    </div>
+                    <button class="btn">Register</button>
+                </form>
+            </div>`;
     }
     addEventListeners() {
         document
